cfed4e4aa281fdeb1efa1716182ca237980c5a68
Antipov Sergey <seregad544@gmail.com>
2022-12-04 17:11:52 +0300

    change head

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -1,19 +1,16 @@
 import { React, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { AuthorizationContext } from '../AuthorizationContext';
 
 function Head() {
   const { authorization, logout } = useContext(AuthorizationContext);
   const { t } = useTranslation();
-  const navigate = useNavigate();
-  const goHome = () => navigate('/');
 
   return (
     <header className="shadow-sm navbar navbar-expand-lg navbar-light gradient black">
       <div className="container">
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-        <div role="button" className="navbar-brand text-light" tabIndex={0} onClick={goHome}>Hexlet Chat</div>
+        <Link className="navbar-brand text-light" to="/">Hexlet Chat</Link>
         {(authorization.status === true) ? <button onClick={() => logout()} type="button" className="btn btn-outline-primary">{t('home.button')}</button> : null}
       </div>
     </header>
